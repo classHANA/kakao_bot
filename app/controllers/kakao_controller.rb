8aeed95fe65474fefc5cd5ee579b3d0c1fe2184f
changhee <och8808@naver.com>
2017-12-29 10:29:19 +0900

    keyboard+message 기능추가

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -8,5 +8,42 @@ class KakaoController < ApplicationController
   end
 
   def message
+
+  	# 사용자가 보내준 텍스트를 그대로 다시 말하기
+
+  	user_message = params[:content]
+
+
+
+  	return_message = {
+  		:message => {
+  			:text => user_message 
+  		}
+
+  	}
+
+
+  	render json: return_message
+
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
