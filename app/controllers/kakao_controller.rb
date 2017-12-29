ae69ca39d7a2da2ecf04c1d8de3229d3ffb7b68d
changhee <och8808@naver.com>
2017-12-29 11:12:43 +0900

    lotto menu

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -13,8 +13,18 @@ class KakaoController < ApplicationController
 
   	user_message = params[:content]
 
-
-
+  	# 로또
+  	# 메뉴
+  	# 다른 명령어가 들어왔을때 
+  	# => ㅠㅠ 알수없는 명령어 입니다.
+  	if user_message == "로또"
+  		user_message = (1..45).to_a.sample(6).to_s
+  	elsif user_message == "메뉴" 
+  		user_message = ["20층","시골집","다이어트","편의점","시래기"]
+  	else
+  		user_message = "지금 사용가능한 명령어는 <메뉴>,<로또> 입니다."
+  	end
+  		
   	return_message = {
   		:message => {
   			:text => user_message 
