1cc94dc1d189e63c847df099314ad10a3f932588
changhee <och8808@naver.com>
2017-12-29 15:21:08 +0900

    parser module

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,3 +1,4 @@
+require 'parser'
 
 class KakaoController < ApplicationController
   def keyboard
@@ -24,35 +25,21 @@ class KakaoController < ApplicationController
   	elsif user_message == "메뉴" 
   		return_text = ["20층","시골집","다이어트","편의점","시래기"].sample
   	elsif user_message == "고양이"
-  		#고양이 사진 보여주기
+  		image = true
+  		animal = Parser::Animal.new
   		return_text = "나만 고양이 없어"
-  		image = true
-  		url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
-  		cat_xml = RestClient.get(url)
-  		doc = Nokogiri::XML(cat_xml)
-  		cat_url = doc.xpath("//url").text
-  	elsif user_message == "영화"
-  		image = true
-  		url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
-  		movie_html = RestClient.get(url)
-  		doc = Nokogiri::HTML(movie_html)
+  		img_url = animal.cat
 
-  		movie_title = Array.new
-  		movie_info = Hash.new
-  		doc.css("ul.lst_detail_t1 dt a").each do |title|
-  			movie_title << title.text
-  		end
-		doc.css("ul.lst_detail_t1 li").each do |movie|
-  			movie_info[movie.css("dl dt.tit a").text] = {
-  				:url => movie.css("div.thumb img").attribute('src').to_s,
-  				:star => movie.css("dl.info_star span.num").text
-  			}
-  		end
-  		sample_movie = movie_title.sample
-  		return_text = sample_movie + " " + movie_info[sample_movie][:star]
-  		cat_url = movie_info[sample_movie][:url]
+  	elsif user_message == "영화"
+  		
+  		image = true
+  		naver_movie = Parser::Movie.new
+  		naver_movie_info = naver_movie.naver
+  		return_text = naver_movie_info[0]
+  		img_url = naver_movie_info[1]
+
   	else
-  		return_text = "지금 사용가능한 명령어는 <메뉴>,<로또> 입니다."
+  		return_text = "로또,고양이,영화,메뉴"
   	end
   		
   	# return_message = {
@@ -69,7 +56,7 @@ class KakaoController < ApplicationController
   		:message => {
   			:text => return_text,
   			:photo => {
-  				:url => cat_url,
+  				:url => img_url,
   				:width => 640,
   				:height => 480
   			}
