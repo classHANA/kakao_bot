1d6f792f1992afe4a106ae26329032b1c9d03622
changhee <och8808@naver.com>
2017-12-29 13:56:14 +0900

    movie

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -31,6 +31,26 @@ class KakaoController < ApplicationController
   		cat_xml = RestClient.get(url)
   		doc = Nokogiri::XML(cat_xml)
   		cat_url = doc.xpath("//url").text
+  	elsif user_message == "영화"
+  		image = true
+  		url = "http://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve"
+  		movie_html = RestClient.get(url)
+  		doc = Nokogiri::HTML(movie_html)
+
+  		movie_title = Array.new
+  		movie_info = Hash.new
+  		doc.css("ul.lst_detail_t1 dt a").each do |title|
+  			movie_title << title.text
+  		end
+		doc.css("ul.lst_detail_t1 li").each do |movie|
+  			movie_info[movie.css("dl dt.tit a").text] = {
+  				:url => movie.css("div.thumb img").attribute('src').to_s,
+  				:star => movie.css("dl.info_star span.num").text
+  			}
+  		end
+  		sample_movie = movie_title.sample
+  		return_text = sample_movie + " " + movie_info[sample_movie][:star]
+  		cat_url = movie_info[sample_movie][:url]
   	else
   		return_text = "지금 사용가능한 명령어는 <메뉴>,<로또> 입니다."
   	end
