afa71d8730ea5bf1491eb8d9587bab7bbc63a287
changhee <och8808@naver.com>
2017-12-29 11:37:13 +0900

    cat

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,3 +1,4 @@
+
 class KakaoController < ApplicationController
   def keyboard
   	home_keyboard = {
@@ -12,29 +13,61 @@ class KakaoController < ApplicationController
   	# 사용자가 보내준 텍스트를 그대로 다시 말하기
 
   	user_message = params[:content]
-
+  	return_text = "임시텍스트"
+  	image = false
   	# 로또
   	# 메뉴
   	# 다른 명령어가 들어왔을때 
   	# => ㅠㅠ 알수없는 명령어 입니다.
   	if user_message == "로또"
-  		user_message = (1..45).to_a.sample(6).to_s
+  		return_text = (1..45).to_a.sample(6).to_s
   	elsif user_message == "메뉴" 
-  		user_message = ["20층","시골집","다이어트","편의점","시래기"].sample
+  		return_text = ["20층","시골집","다이어트","편의점","시래기"].sample
+  	elsif user_message == "고양이"
+  		#고양이 사진 보여주기
+  		image = true
+  		url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
+  		cat_xml = RestClient.get(url)
+  		doc = Nokogiri::XML(cat_xml)
+  		cat_url = doc.xpath("//url").text
   	else
-  		user_message = "지금 사용가능한 명령어는 <메뉴>,<로또> 입니다."
+  		return_text = "지금 사용가능한 명령어는 <메뉴>,<로또> 입니다."
   	end
   		
-  	return_message = {
-  		:message => {
-  			:text => user_message 
-  		}
+  	# return_message = {
+  	# 	:message => {
+  	# 		:text => user_message 
+  	# 	}
 
+  	# }
+  	home_keyboard = {
+  		:type => "text"
   	}
 
+  	return_message_with_img = {
+  		:message => {
+  			:text => return_text,
+  			:photo => {
+  				:url => cat_url,
+  				:width => 640,
+  				:height => 480
+  			}
+  			},
+  		:keyboard => home_keyboard
+  	}
+  	
+  	return_message = {
+  		:message => {
+  			:text => return_text,
+  			},
+  		:keyboard => home_keyboard
+  	}
 
-  	render json: return_message
-
+  	if image
+  		render json: return_message_with_img
+  	else
+  		render json: return_message
+  	end
   end
 end
 
